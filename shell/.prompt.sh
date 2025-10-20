ae06f5d85a2681078c6af1636ad7f034c2381966
Andrew Speirs <andrew@speirs.dev>
2025-10-20 22:15:33 +0100

    fix: get prompt to play nice on zsh

--- shell/.prompt.sh
+++ shell/.prompt.sh
@@ -7,7 +7,8 @@ function parse_git_branch() {
 # Check if the ZSH_VERSION variable is set. If it is, we are in Zsh.
 if [ -n "$ZSH_VERSION" ]; then
   # This is ZSH. Use Zsh's native prompt syntax.
-  PROMPT='%F{cyan}%~%f %F{yellow}$(parse_git_branch)%f $ '
+  setopt PROMPT_SUBST
+  export PROMPT='%F{cyan}%~%f %F{yellow}$(parse_git_branch)%f $ '
 else
   # This is BASH. Use Bash's PS1 syntax.
   export PS1="\[\033[36m\]\w\[\033[00m\] \[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
