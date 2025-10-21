7b293778795c45810b82be4d532cef6ff3e8e7bb
Andy Speirs <andy.speirs@proseonpixels.com>
2025-10-21 11:46:30 +0100

    fix: show host on macos and in ssh session

--- shell/.prompt.sh
+++ shell/.prompt.sh
@@ -1,16 +1,37 @@
+if [[ "$OSTYPE" == darwin* ]] || [[ -n "$SSH_CONNECTION" || -n "$SSH_CLIENT" || -n "$SSH_TTY" ]]; then
+  SHOULD_SHOW_HOST=true
+else
+  SHOULD_SHOW_HOST=false
+fi
+
 function parse_git_branch() {
-  local branch
-  branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-  [[ -n "$branch" ]] && echo "($branch)"
+  if [ -d .git ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    local BRANCH
+    BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null)
+    [[ -n "$BRANCH" ]] && echo "($BRANCH)"
+  fi
 }
 
-# Check if the ZSH_VERSION variable is set. If it is, we are in Zsh.
 if [ -n "$ZSH_VERSION" ]; then
-  # This is ZSH. Use Zsh's native prompt syntax.
   setopt PROMPT_SUBST
-  export PROMPT='%F{cyan}%~%f $ '
-  export RPROMPT='%F{yellow}$(parse_git_branch)%f'
+
+  if $SHOULD_SHOW_HOST; then
+    LEFT_PREFIX='%n@%m '
+  else
+    LEFT_PREFIX=''
+  fi
+
+  function git_branch_prompt_zsh() {
+    local branch_output
+    branch_output=$(parse_git_branch)
+    [[ -n "$branch_output" ]] && echo "%F{yellow}${branch_output}%f"
+  }
+
+  # Set PROMPT: [user@host] [/current/path] $
+  export PROMPT="${LEFT_PREFIX}%F{cyan}%~%f $ "
+  # Set RPROMPT: [(git-branch)]
+  export RPROMPT='$(git_branch_prompt_zsh)'
 else
-  # This is BASH. Use Bash's PS1 syntax.
+  # PS1: [cyan][/current/path] [yellow][(git-branch)] $
   export PS1="\[\033[36m\]\w\[\033[00m\] \[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
 fi
