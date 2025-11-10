e3e20776565f55571223e0be064868b1200a68c8
Andrew Speirs <andrew@speirs.dev>
2025-11-10 17:39:45 +0000

    feat: move snacks explorer to the right

---
+++ loader/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = false,
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right",
+            }
+          }
+        },
+      },
+    },
+  },
+}
