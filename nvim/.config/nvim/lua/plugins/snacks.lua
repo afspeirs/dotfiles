7bb11a7985998fcf94d447f98a1f41428e4c05cc
Andrew Speirs <andrew@speirs.dev>
2026-08-02 21:42:25 +0100

    fix: update buffer keymaps

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -18,13 +18,14 @@ return {
     -- Buffer
     { "<leader><leader>", function() Snacks.picker.buffers() end, desc = "View Buffers" },
     { "<leader>bb", function() Snacks.picker.buffers() end, desc = "View Buffers" },
-    { "<leader>bn", "<cmd>enew<cr>", desc = "New Empty Buffer" },
+    { "<leader>bc", "<cmd>enew<cr>", desc = "New Empty Buffer" },
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
     { "<leader>bD", function() Snacks.bufdelete.other() end, desc = "Delete All Other Buffers" },
     { "<leader>bs", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
     { "<leader>bS", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     { "<leader>bp", "<cmd>bprevious<cr>", desc = "Previous Buffer" },
     { "<leader>bl", "<cmd>bnext<cr>", desc = "Next Buffer" },
+    { "<leader>bn", "<cmd>bnext<cr>", desc = "Next Buffer" },
 
     -- Git
     { "<leader>gg", function() Snacks.lazygit() end, desc = "LazyGit" },
